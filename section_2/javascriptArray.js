1ef0002b30ac868645be17194e01479172b5e01e
Rohan Dongre <dongrerohan421@gmail.com>
2019-07-04 20:07:19 -0400

    Loop over an Array

--- section_2/javascriptArray.js
+++ section_2/javascriptArray.js
@@ -7,3 +7,8 @@ var first = fruits[0];
 console.log(first);
 var last = fruits[fruits.length - 1];
 console.log(last);
+
+//Loop over an Array
+fruits.forEach(function (item, index, array) {
+    console.log("item: " + item, ", index: " + index);
+});
