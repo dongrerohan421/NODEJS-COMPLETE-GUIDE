e1a6558a3b479a4738eb3691ed5388a197c43785
Rohan Dongre <dongrerohan421@gmail.com>
2019-07-06 17:23:52 -0400

    JavaScript Constructor intro

---
+++ section_2/constructor.js
@@ -0,0 +1,33 @@
+// The constructor property returns a reference to the Object constructor
+// function that created the instance object.Note that the value of this property is a reference to the
+// function itself, not a string containing the
+// function 's name. The value is only read-only for primitive values such as 1, true and "test".
+
+// Description:
+// All objects(with the exception of objects created with Object.create(null)) will have a constructor property.
+// Objects created without the explicit use of a constructor
+// function (i.e.the object and array literals) will have a constructor property that points to the Fundamental Object constructor type
+// for that object.
+
+var o = {};
+o.constructor() === Object; //true
+console.log(o.constructor());
+
+var o = new Object;
+o.constructor === Object; //true
+console.log(o.constructor());
+
+var a = [];
+a.constructor === Array; // true
+console.log(a.constructor);
+
+var a = new Array;
+a.constructor === Array; // true
+console.log(a.constructor);
+
+var n = new Number(3);
+n.constructor === Number; // true
+console.log(n.constructor());
+
+
+// Examples
