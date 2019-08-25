83218106650a7a05ac1f7ea66503f5c493974345
Rohan Dongre <dongrerohan421@gmail.com>
2019-08-25 15:17:01 -0400

    add ejs view engine

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,10 @@ router.get('/', (req, res, next) => {
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
     });
 });
 
